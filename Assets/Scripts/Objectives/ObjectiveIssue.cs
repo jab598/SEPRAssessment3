c1101258a8a2695c479f4b19783657f36c60323a
Jack <jab598@york.ac.uk>
2016-01-25 15:54:21 +0000

    Ignore halifax comments, thats langwith

---
+++ Assets/Scripts/Objectives/ObjectiveIssue.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectiveIssue : MonoBehaviour {
+
+	public bool issueOnTrigger = true;
+	public bool issueOnCollision = false;
+
+	public string missionName;
+	public List<string> missionParts = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D c) {
+		if (issueOnTrigger) {
+			Objective newObj = new Objective (missionName);
+			foreach(string s in missionParts) {
+				newObj.addPart(s);
+			}
+			ObjectiveHandler.instance.objectives.Add (newObj);
+			ObjectiveHandler.instance.UpdateUI();
+		}
+	}
+}
